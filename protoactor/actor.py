6fd25583aa8cddd9a8ec591db6c5c2451988beb1
ipinak <giannis.pin@gmail.com>
2017-03-05 10:51:09 +0000

    add actor creation functions

--- protoactor/actor.py
+++ protoactor/actor.py
@@ -3,29 +3,29 @@ from typing import Callable
 
 from . import process_registry, props, context, pid
 
+
 class Actor(metaclass=ABCMeta):
     @abstractmethod
     async def receive(self, context: context.AbstractContext) -> None:
         pass
 
+    @staticmethod
+    def from_func(receive) -> 'Props':
+        return Actor.from_producer(receive)
 
-def from_producer(producer: Callable[[], Actor]) -> 'Props':
-    return props.Props(producer=producer)
+    @staticmethod
+    def spawn(properties: props.Props) -> 'PID':
+        name = ProcessRegistry().next_id()
+        return Actor.spawned_name(properties, name)
 
+    @staticmethod
+    def spawned_name(properties: props.Props, name: str) -> 'PID':
+        return properties.spawn(name)
 
-def from_func(receive) -> 'Props':
-    pass
-
-
-def spawn(props: 'Props') -> pid.PID:
-    return props.spawn(process_registry.ProcessRegistry().next_id())
-
-
-def spawn_prefix(props: 'Props', prefix: str):
-    pass
-
-
-def spawn_named(props: 'Props', name: str):
-    pass
-
-
+    @staticmethod
+    def spawn_prefix(properties: props.Props, prefix: str) -> 'PID':
+        name = "%(prefix)s%(next_id)s" % {
+            "prefix": prefix,
+            "next_id": ProcessRegistry().next_id()
+        }
+        return Actor.spawned_name(properties, name)
