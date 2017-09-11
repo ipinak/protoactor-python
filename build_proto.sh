ccee557dcba51df9ec431071d4fca76fecc9793e
ipinak <giannis.pin@gmail.com>
2017-09-11 22:07:15 +0200

    added build script for protobufs

---
+++ build_proto.sh
@@ -0,0 +1,18 @@
+
+input=$1
+
+case $input in
+    remote)
+        cd protoactor/remote
+        echo "Compiling remote messages"
+        # python -m grpc_tools.protoc -I. -I.. --python_out=. --grpc_python_out=. protoactor/remote/protos.proto
+        ;;
+    actor)
+        cd protoactor
+        echo "Compiling actor messages"
+        protoc protos_pb2.proto -I. --python_out=.
+        ;;
+    *)
+        echo "./build_proto.sh [remote|actor]"
+        ;;
+esac
